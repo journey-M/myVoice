261fe238e4be533e394db798b0cda3cd713b162a
gwjgg <1485765145@qq.com>
2018-04-12 10:32:00 +0800

    init project

---
+++ app/src/main/java/dev/gwj/myvoice/MainActivity.java
@@ -0,0 +1,19 @@
+package dev.gwj.myvoice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dd on 18-4-12.
+ */
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
